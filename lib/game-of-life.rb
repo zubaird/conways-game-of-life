81dc578f77d52c53c1745e185f34ba96da32e0a6
Zubair Desai <zubair.desai@gmail.com>
2014-12-25 17:27:46 -0800

    Cell can detect neighbours, knows its location, dies when area is underpopulated

--- lib/game-of-life.rb
+++ lib/game-of-life.rb
@@ -1,34 +1,120 @@
 require 'pry'
 
 class LifeBoard
-
-	attr_accessor :number_of_columns, :number_of_rows
+	attr_accessor :number_of_columns, :number_of_rows, :array_grid
 
 	def initialize(number_of_rows = 3, number_of_columns = 3)
 		@number_of_rows = number_of_rows
 		@number_of_columns = number_of_columns
+		build_array_for_grid
+		set_cells_row_address
 	end
-
 
 	def array_columns
 		build_columns = Array.new
-
-		number_of_columns.times do 
-			build_columns << "#"
+		i = 0
+		while i < number_of_columns do 
+			build_columns << Cell.new
+			i += 1
 		end
 		build_columns
 	end
 
-
 	def build_array_for_grid
-		array_grid = Array.new
+		@array_grid = Array.new
 
 		number_of_rows.times do 
-			array_grid << array_columns
+			@array_grid << array_columns
+		end
+		@array_grid
+	end
+
+	def set_cells_row_address
+		i = 0
+		j = 0
+		while i < @array_grid.size do
+			j = 0
+			while j < @array_grid[i].size do 
+				@array_grid[i][j].cell_address = [i,j]
+				j += 1
+			end	
+			i += 1
 		end
 
-		array_grid
+		@array_grid
+	end
+end
+
+class Cell 
+
+	attr_accessor :cell_address, :alive, :dead
+
+	def initialize(cell_address = [1,1])
+		alive
+		@cell_address = cell_address
+	end
+
+	def current_cell_in(array_grid)
+		column = @cell_address[0]
+		row = @cell_address[1]
+		current_cell = array_grid[row][column]
+	end
+
+	def alive
+		alive = true
+		dead = false
+		"#"
+	end
+
+	def dead
+		dead = true
+		alive = false
+		"-"
+	end
+
+	def cell_address=(value)
+		@cell_address = value
 	end
 
 
+	def neighbours_in(array_grid)
+		neighbour_group = []
+		
+		column = @cell_address[0] + 1
+		row = @cell_address[1] + 1
+
+		p column
+		p row
+
+		i = 0
+		j = 0
+
+		while i <= 2 do
+			j = 0
+			while j <= 2 do
+				neighbour_group << array_grid[row - i][column - j].cell_address
+				j += 1
+			end
+			i += 1
+		end
+		neighbour_group
+	end
+
+	def under_population(array_grid)
+		
+		alive_neighbours = 0
+		
+
+		neighbours_in(array_grid).each do |neighbouring_cell_address|
+			column = neighbouring_cell_address[0]
+			row = neighbouring_cell_address[1]
+			alive_neighbours += 1 if array_grid[column][row].alive == true
+		end
+
+		current_cell_in(array_grid).dead if alive_neighbours <= 1
+	end
+
 end
+
+
+
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -12,7 +12,6 @@ describe LifeBoard do
 		expect(game.number_of_columns).to eq(10)
 	end
 
-
 	it "has a rows count" do
 		game = LifeBoard.new
 
@@ -31,29 +30,125 @@ describe LifeBoard do
 	it "builds columns" do
 		game = LifeBoard.new(10,10)
 
-		expected = ["#","#","#","#","#","#","#","#","#","#"]
+		expected = 10
 		
-		expect(game.array_columns).to eq(expected)
+		expect(game.array_columns.size).to eq(expected)
 	end
 
-	it "builds a graphical grid based on rows x columns"  do
+	# xit "builds a graphical grid based on rows x columns"  do
+	# 	game = LifeBoard.new(10,10)
+
+	# 	expected = [
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 		["#","#","#","#","#","#","#","#","#","#"],
+	# 	]
+
+	# 	expect(game.make_all_alive).to eq(expected)
+	# end
+
+	it "sets the cell row address" do
+
 		game = LifeBoard.new(10,10)
 
-		expected = [
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-			["#","#","#","#","#","#","#","#","#","#"],
-		]
+		grid_address = [0,0]
+		first_cell_address = game.array_grid[0][0].cell_address
 
-		expect(game.build_array_for_grid).to eq(expected)
+		expect(first_cell_address).to eq(grid_address)
 	end
 
+end
+
+
+
+
+describe Cell do 
+
+	before do
+		@game = LifeBoard.new(10,10)
+		@cell = Cell.new
+		@grid_rows = @game.array_grid
+	end
+
+	def each_square_is_a_cell?
+		
+
+		is_a_single_cell = true
+
+		@grid_rows.each do |row|
+			row.each do |square|
+				is_a_single_cell = false if square.class != Cell
+			end
+		end
+
+		return is_a_single_cell
+	end
+
+	it "occupies a single square on the LifeBoard" do
+		expect(each_square_is_a_cell?).to eq(true)
+	end
+
+	it "looks like '#' when it is #alive" do
+		expect(@cell.alive).to eq("#")
+	end
+
+	it "looks like '-' when it is #dead" do
+		expect(@cell.dead).to eq("-")
+	end
+
+	it "knows its cell_address" do
+		last_cell = @grid_rows[9][9]
+		
+		expect(last_cell.cell_address).to eq([9,9])
+	end
+
+	it "knows its neighbours" do
+
+		neighbours = @grid_rows[5][5].neighbours_in(@grid_rows)
+
+		expected = [
+			[6,6],
+			[6,5],
+			[6,4],
+			[5,6],
+			[5,5],
+			[5,4],
+			[4,6],
+			[4,5],
+			[4,4],
+		]
+
+		expect(neighbours).to eq(expected)
+	end
+
+	describe "#under_population" do 
+		it 'cells die when it has 1 or less live neighbours' do
+
+			die = @grid_rows[5][5].under_population(@grid_rows)
+
+			expect(die).to eq("-")
+
+		end
+	end
+
+	describe "#over_population" do 
+		it 'cells die when it has 4 or more live neighbours'
+	end
+	
+	describe "#lives_on" do 
+		it 'cells live for another generation when it has 2 or 3 live neighbours'
+	end
+
+
+	describe "#is_born" do 
+		it 'cell is born or continues to live if it has exactly 3 live neighbours'
+	end
 
 end
